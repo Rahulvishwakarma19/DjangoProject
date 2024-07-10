ba225de5386396f795e6e12f7935be63fa6b95c5
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-07-10 21:09:58 +0530

    sum exeptions

--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
-from django.shortcuts import get_list_or_404,redirect
+from django.shortcuts import get_object_or_404 ,redirect
 
 # Create your views here.
 def index(request):
@@ -24,7 +24,7 @@ def tweet_create(request):
     return render(request,'tweet_form.html',{'form':form})
 
 def tweet_edit(request, tweet_id):
-    tweet = get_list_or_404(Tweet, pk=tweet_id, user = request.user)
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
     if request.method == "POST":
         form = TweetForm(request.POST, request.FTLES, instance=tweet)
         if form.is_valid():
