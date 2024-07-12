fd7c77971dda38b08ca98718af684e14f5ec5af4
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-07-12 21:41:43 +0530

    add ragister views

--- chaiheadq/chaiheadq/settings.py
+++ chaiheadq/chaiheadq/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static') ]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
+LOGIN_REDIRECT_URL = '/tweet/'
+LOGOUT_REDIRECT_URL = '/tweet/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- chaiheadq/tweet/urls.py
+++ chaiheadq/tweet/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('<int:tweet_id>/edit/',tweet_edit,name='tweet_edit'),
 
     path('<int:tweet_id>/delete/',tweet_delete,name='tweet_delete'),
+
+    path('register/',register,name='register'),
 ] 
